33b76e667d8a4612b92774da181b4371d60ddfd5
= <=>
2016-09-28 08:01:40 -0700

    first commit

---
+++ guessing.py
@@ -0,0 +1,25 @@
+import random 
+
+numberToGuess = random.randrange(0,50)
+
+tries = 3
+
+while tries >= 0:
+
+	if tries == 0:
+		print "you lose!!! my numer was %s" %numberToGuess
+		break
+	
+	print "you have %s tries left" %tries
+
+	guess = input("guess my number")
+	
+	if guess > numberToGuess:
+		print "my number is smaller"
+		tries -= 1
+	elif guess < numberToGuess :
+		print "my number is bigger"
+		tries -=1
+	else:
+		print "you win!"
+
