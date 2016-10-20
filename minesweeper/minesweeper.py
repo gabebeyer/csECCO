35c33b7fbec2600d278f093ac1b4961798867527
gabe <beyergabe@gmail.com>
2016-10-20 11:44:58 -0700

    asdf

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,6 +1,7 @@
 from Tkinter import * 
 import tkMessageBox 
 import random
+import functools
 import numpy as np
 
 
@@ -12,37 +13,44 @@ class Minesweeper():
 
         self.buttons = [] 
 
-        #add buttons to array
+        self.bombPercent = 20
+
+        #add buttons array
+        #along with info about button
+        # button , isBomb , location
         for x in range(8):
-
             row = []
-
-            for i in range(8):
-                
-                row.append( [Button(master, text=str((x,i)) ) ])
-
+            for i in range(8):           
+                row.append([Button(master, text=str((x,i))) ,
+                            1 if random.randint(0,100) < self.bombPercent else 0,
+                            (x,i)
+                    ])
             self.buttons.append(row)
 
 
         rowVar = 0
         #put buttons on screen
+        #and sets up function for buttons
         for row in self.buttons:
-            
             colVar = 0
-            
             for button in row:
-                
+                x = (rowVar , colVar)
+
                 button[0].grid(row = rowVar, column = colVar)
+                
+                self.buttons[rowVar][colVar][0].bind('<Button-1>',functools.partial(self.lClickWrap,param=self.buttons[rowVar][colVar]))
 
                 colVar += 1
-
             rowVar += 1
-        
-        for row in self.buttons:
-            
-            for b in row:
 
-                print(b[0].config('text'))
+
+    def lClickWrap(self,event,param):
+        print "hello " + str(param[2])
+
+
+
+ 
+
 
 
 def main():
