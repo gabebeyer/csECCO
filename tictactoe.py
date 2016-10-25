10fc07d0365028497e85ad737deeef71ffa3cc5d
Gabe Beyer <gabebeyer@Gabes-MacBook-Pro.local>
2016-10-25 11:30:48 -0700

    tac

--- tictactoe.py
+++ tictactoe.py
@@ -12,9 +12,7 @@ class App:
         self.customFont = tkFont.Font(family="Helvetica", size=44)
         self.turn = 0
         self.gamemode = 2
-        #self.testNum = 0
-                    #center
-        # square : [ x, y , filled , owner]
+
         self.square_info = {1: [33,33,0,''],
                             2: [100,33,0,''],
                             3: [166,33,0,''],
@@ -24,13 +22,9 @@ class App:
                             7: [33,166,0,''],
                             8: [100,166,0,''],
                             9: [166,166,0,'']}       
-        #		1    2    3
-        #
-        #
-        #       4    5    6
-        #
-        #       
-        #       7    8    9
+
+     
+
 
         self.canvas = Canvas(master, width=200, height=200)
         self.canvas.bind("<Button-1>", self.mouse_click)
@@ -55,24 +49,18 @@ class App:
     
     ##full restart
     def end_game(self,winner):
-                
         newx = self.master.winfo_x()
         newy = self.master.winfo_y()
-
         tkMessageBox.showinfo("Winner", "The winner is: " + winner + "!!!", parent = self.master)
-
         self.canvas.delete("all")
-  
         self.master.destroy()
         main(newx,newy)
 
 
     def check_for_winner(self):        
-        
         square1,square2,square3 = self.square_info[1][3],self.square_info[2][3],self.square_info[3][3]        
         square4,square5,square6 = self.square_info[4][3],self.square_info[5][3],self.square_info[6][3]
-        square7,square8,square9 = self.square_info[7][3], self.square_info[8][3],self.square_info[9][3]
-    
+        square7,square8,square9 = self.square_info[7][3], self.square_info[8][3],self.square_info[9][3]    
         if square1 == square2 == square3 != '': self.end_game(square1)
         if square4 == square5 == square6 != '': self.end_game(square4)
         if square7 == square8 == square9 != '': self.end_game(square7)
@@ -86,9 +74,33 @@ class App:
         for x in xrange(1,10):
             self.testNum +=  self.square_info[x][2]
         if self.testNum == 9:
-            self.end_game("You both lose!!")
+            self.end_game("(=ↀωↀ=)")
 
     def cpu_turn(self, *args):
+
+        threats = [[self.square_info[1],self.square_info[2],self.square_info[3]],
+                    [self.square_info[4],self.square_info[5],self.square_info[6]],
+                    [self.square_info[7],self.square_info[8],self.square_info[9]],
+                    [self.square_info[1],self.square_info[4],self.square_info[7]],
+                    [self.square_info[2],self.square_info[5],self.square_info[8]],
+                    [self.square_info[3],self.square_info[6],self.square_info[9]],
+                    [self.square_info[1],self.square_info[2],self.square_info[3]],
+                    [self.square_info[1],self.square_info[2],self.square_info[3]]]
+
+        for threat in threats:
+            number = 0
+            for box in threat:
+                if box[2] == 1 and box[3] == 'x':
+                    number += 1 
+            if number == 2:
+                for box in threat:
+                    if box[2] == 0:
+                        for key in self.square_info.keys():
+                            if  self.square_info[key] == box :
+                                self.draw_mark('o',key)
+                                self.square_info[key][2] = 1
+                                self.square_info[key][3] = 'o'
+                                return
         #center
         if self.square_info[5][2] == 0:
             self.draw_mark('o',5)
@@ -111,44 +123,28 @@ class App:
                 self.square_info[i][2] = 1
                 self.square_info[i][3] = 'o'
                 return
-     
-
             
     def mouse_click(self,event):
-
         square = self.coord_to_square(event.x, event.y)
-
         if self.gamemode == 1:
-
             if self.turn == 0 and self.square_info[square][2] == 0:
                 self.draw_mark('x',square)
                 self.square_info[square][2] = 1
                 self.square_info[square][3] = 'x'
                 self.turn = 1
-
             elif self.turn == 1 and self.square_info[square][2] == 0:
                 self.draw_mark('o',square)
                 self.square_info[square][2] = 1
                 self.square_info[square][3] = 'o'
                 self.turn = 0
-
         elif self.gamemode == 2:
-
             if self.square_info[square][2] == 0:
-
                 self.draw_mark('x', square)
                 self.square_info[square][2] = 1
                 self.square_info[square][3] = 'x'
-
-
                 self.check_for_winner()
-
-
                 self.cpu_turn(self)
-
-
         self.check_for_winner()
-
 
 
     def draw_level(self):    
